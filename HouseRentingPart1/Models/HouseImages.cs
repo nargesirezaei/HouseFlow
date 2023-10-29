96b4954e81bacb88c074cde4fc24ed91793e9937
Merri04 <merrisium15@gmail.com>
2023-10-29 15:05:03 +0100

    added a comment to everything

--- HouseRentingPart1/Models/HouseImages.cs
+++ HouseRentingPart1/Models/HouseImages.cs
@@ -6,9 +6,11 @@ namespace HouseFlowPart1.Models
 {
     public class HouseImages
     {
+        // 'Id' is a unique identifier for an image.
         public ObjectId Id { get; set; }
+        // 'HouseId' associates the image with a specific house, linking it to property listings.
         public ObjectId HouseId { get; set; }
-
+        // 'ImageUrl' stores the URL of the image, and it is marked as required and not nullable
         [Required]
         [NotNull]
         public string ImageUrl { get; set; } = "";
